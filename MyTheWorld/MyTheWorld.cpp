fe81523f71403d3509384d52c3e2bff1113b500c
rogerzhu0710 <rogerzhu0710@126.com>
2016-06-27 17:01:33 +0800

    2016-06-27
    use Create thread and critical section

---
+++ MyTheWorld/MyTheWorld.cpp
@@ -0,0 +1,160 @@
+// MyTheWorld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+
+using namespace std;
+
+void MoveOutputToPos(int x,int y,char* c,bool bString);
+
+HANDLE hOut=GetStdHandle(STD_OUTPUT_HANDLE);
+
+struct Planet
+{
+    int type;
+    int resource;
+    int speed;
+    int index;
+};
+
+CRITICAL_SECTION cs;
+
+const struct Planet& InitPlanet(int type, int resource,int speed,int index);
+void PlanetSpin(const struct Planet &p,int spinInterval,int picType);
+void PlanetMove(struct Planet &p);
+
+DWORD WINAPI ThreadProc(LPVOID lpParameter);
+int nMaxResource = 0;
+
+int _tmain(int argc, _TCHAR* argv[])
+{  
+    CONSOLE_SCREEN_BUFFER_INFO bInfo; 
+    GetConsoleScreenBufferInfo(hOut, &bInfo );
+    InitializeCriticalSection(&cs);
+
+    nMaxResource = bInfo.dwSize.X;
+    struct Planet homePlanet = InitPlanet(1,nMaxResource,0,0); 
+    struct Planet Planet1 = InitPlanet(0,0,1,1); 
+    struct Planet Planet2 = InitPlanet(0,0,1,2); 
+
+    int nSpinType = 0;
+    HANDLE  hThread1 = CreateThread(0, 
+                             16 * 1024L, 
+                             ThreadProc, 
+                             &Planet1, 
+                             STACK_SIZE_PARAM_IS_A_RESERVATION, 
+                             NULL);
+ 
+    HANDLE  hThread2 = CreateThread(0, 
+                             16 * 1024L, 
+                             ThreadProc, 
+                             &Planet2, 
+                             STACK_SIZE_PARAM_IS_A_RESERVATION, 
+                             NULL);
+
+    while(true)
+    {
+        PlanetSpin(homePlanet,80,nSpinType);  
+        nSpinType++;
+    }
+     
+    DeleteCriticalSection(&cs);
+    system("pause");
+	return 0;
+}
+
+void PlanetMove(struct Planet &p)
+{ 
+    p.resource++;
+    char cLine[100];
+    memset(cLine,0,100*sizeof(char));
+    memset(cLine,'-',p.resource*sizeof(char));
+
+    if( p.resource == nMaxResource ) 
+    {
+        p.resource = 0;
+        memset(cLine,0,100*sizeof(char)); 
+    } 
+    
+    MoveOutputToPos(0,p.index*3+1,cLine,true); 
+}
+
+void PlanetSpin(const struct Planet &p,int spinInterval,int picType)
+{
+    char spin[4] = {'|','/','-','\\'}; 
+
+    if( p.type == 1 )
+    {   
+        MoveOutputToPos(0,0,&spin[picType%4],false); 
+        MoveOutputToPos(0,1,&spin[picType%4],false); 
+        MoveOutputToPos(1,0,&spin[picType%4],false); 
+        MoveOutputToPos(1,1,&spin[picType%4],false);
+       
+        ::Sleep(spinInterval);
+    }
+    else
+    { 
+        char sep = '*';
+        
+        MoveOutputToPos(0,p.index*3-1,&sep,false);  
+        MoveOutputToPos(0,p.index*3,&sep,false);    
+
+
+        MoveOutputToPos(p.resource,p.index*3+1,&spin[picType%4],false); 
+  
+        ::Sleep(spinInterval);
+    }
+}
+
+const struct Planet& InitPlanet(int type, int resource,int speed,int index)
+{
+    struct Planet p;
+    p.type = type;
+    p.resource = resource;
+    p.speed = speed;
+    p.index = index;
+
+    return p;
+}
+
+void MoveOutputToPos(int x,int y,char* c,bool bString)
+{     
+    EnterCriticalSection(&cs);
+    CONSOLE_SCREEN_BUFFER_INFO csbiInfo; 
+    csbiInfo.dwCursorPosition.X = x;
+    csbiInfo.dwCursorPosition.Y = y;
+
+    if (!SetConsoleCursorPosition(hOut,csbiInfo.dwCursorPosition)) 
+    {
+        printf("SetConsoleCursorPosition error!!!!! \r\n"); 
+         
+        return;
+    }
+
+    if(!bString)
+        printf("%c",c[0]); 
+    else
+        printf("%s",c);
+    LeaveCriticalSection(&cs);
+}
+
+DWORD WINAPI ThreadProc(LPVOID lpParameter)
+{
+    struct Planet* p = (struct Planet*)lpParameter;
+    int nSpinType = 0;
+
+  
+    while(true)
+    {
+        PlanetMove(*p);
+        PlanetSpin(*p,100,nSpinType);
+
+        nSpinType++;
+    }
+        
+    return 1;
+}
